62ea2a92224d60b3fc2850634f4f1df5edb4ff7d
mishkinaa <amishkina@griddynamics.com>
2023-10-07 00:21:22 +0200

    complete exercises

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -11,25 +11,28 @@ import Counter from '../../components/counter'
 // Luckily, it's handled for you by React Testing Library :)
 global.IS_REACT_ACT_ENVIRONMENT = true
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use createRoot to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 act(() => increment.click()))
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 act(() => decrement.click()))
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+beforeEach(() => {
+  document.body.innerHTML = ''
 })
 
-/* eslint no-unused-vars:0 */
+test('counter increments and decrements when the buttons are clicked', () => {
+  const container = document.createElement('div');
+  document.body.append(container);
+
+  const root = createRoot(container);
+  act(() => root.render(<Counter/>));
+
+  const [decrementBtn, incrementBtn] = container.querySelectorAll('button');
+  const messageDiv = container.firstChild.querySelector('div');
+  const click = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0 //left button
+  });
+
+  expect(messageDiv.textContent).toBe('Current count: 0');
+  act(() => incrementBtn.dispatchEvent(click));
+  expect(messageDiv.textContent).toBe('Current count: 1');
+  act(() => decrementBtn.dispatchEvent(click));
+  expect(messageDiv.textContent).toBe('Current count: 0');
+})
--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,21 +2,33 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {act, renderHook} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
-
 test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  const {result} = renderHook(() => useCounter())
+
+  expect(result.current.count).toEqual(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toEqual(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toEqual(0)
 })
 
-/* eslint no-unused-vars:0 */
+test('allows customization of the initial count', () => {
+  const initialCount = 10
+  const {result} = renderHook(useCounter, {initialProps: {initialCount}})
+
+  expect(result.current.count).toEqual(initialCount)
+})
+
+test('allows customization of the step', () => {
+  const step = 5
+  const {result} = renderHook(useCounter, {initialProps: {step}})
+
+  expect(result.current.count).toEqual(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toEqual(step)
+  act(() => result.current.decrement())
+  expect(result.current.count).toEqual(0)
+})
